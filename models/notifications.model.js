fe348b9e3801319fb9dd00e3ff2fc567accb0735
RuiCoutoESMAD <spookyrucohol@gmail.com>
2022-01-20 11:38:04 +0000

    Relation notification with user

--- models/notifications.model.js
+++ models/notifications.model.js
@@ -1,11 +1,11 @@
 const { Sequelize, Model, DataTypes } = require('sequelize'); 
+const { User } = require('./users.model');
 const sequelize = new Sequelize.Sequelize('joaoferr_ESMAPP_21_22_GRP2', 'joaoferr_ESMAPP_21_22_GRP2', 'W9dDquqxfG8PjWdK', {
     host: 'www.joaoferreira.eu', 
     dialect: 'mysql'
 })
 
-
-
+const User = require('./users.model');
 class Notification extends Model {}
 
 Notification.init({
@@ -14,6 +14,9 @@ Notification.init({
     date: DataTypes.DATE,
     deleted: DataTypes.TINYINT
 }, { sequelize, modelName: 'notification'})
+
+Notification.belongsTo(User, {foreignKey: 'userId'});
+User.User.hasMany(Notification, {foreignKey: 'userID'})
 
 
 sequelize.sync().then().catch(error => {
