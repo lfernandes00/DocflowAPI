b3fc29a97ae35b48fb02deebef1cc7c365bb99a4
tiagocouto17 <tiago.couto1745@gmail.com>
2022-01-24 15:07:41 +0000

    notifications swagger

--- routes/notifications.routes.js
+++ routes/notifications.routes.js
@@ -4,6 +4,15 @@ const { body, validationResult } = require('express-validator');
 const controller = require('../controllers/notifications.controller'); 
 const utilities = require('../utilities/utilities');
 
+/**
+ * @route POST /notifications
+ * @group Notifications
+ * @param {object} object.body - Notifications's Credentials - eg. {"name": "Faturas"} 
+ * @returns {object} 201 - New Notification Created
+ * @returns {Error} 400 - Unexpected error
+ * @returns {Error} 500 - Internal Server Error
+ */
+
 router.route('/').post([
     body('userId').notEmpty().escape(),
     body('desc').notEmpty().escape()
@@ -16,6 +25,15 @@ router.route('/').post([
     }
 })
 
+/**
+ * @route GET /notifications/userId
+ * @group Notifications
+ * @returns {object} 200 - An array with all notifications by user
+ * @returns {Error} 404 - 0 notifications found!
+ * @returns {Error} 400 - Unexpected error
+ * @returns {Error} 500 - Internal Server Error
+ */
+
 router.route('/:userId').get(function(req, res) {
     const errors = validationResult(req);
     if (errors.isEmpty()) {
@@ -24,6 +42,21 @@ router.route('/:userId').get(function(req, res) {
         res.status(400).send(erros);
     }
 })
+
+/**
+ * @route PATCH /notifications/{id}
+ * @group Notifications
+ * @param {object} object.body - Notifications's Credentials - eg. {"deleted": 1}
+ * @param {string} id.path - Notification id.
+ * @returns {object} 200 - Notification removed with success!
+ * @returns {Error} 400 - Error while removing notification!
+ * @returns {Error} 400 - Only admin or the user who created this notification can remove it!
+ * @returns {Error} 401 - Invalid Token
+ * @returns {Error} 403 - No token provided
+ * @returns {Error} 404 - Notification not found
+ * @returns {Error} 500 - Internal Server Error
+ * @security Bearer
+ */
 
 router.route('/:notificationId').patch([
     body('deleted').isNumeric().notEmpty().escape()
