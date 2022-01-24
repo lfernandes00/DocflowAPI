a8a132099ef38e0244d3a35b11a05b52e8581daf
tiagocouto17 <tiago.couto1745@gmail.com>
2022-01-24 14:54:15 +0000

    document types swagger

--- routes/documentTypes.routes.js
+++ routes/documentTypes.routes.js
@@ -4,6 +4,15 @@ const { body, validationResult } = require('express-validator');
 const controller = require('../controllers/documentTypes.controller'); 
 const utilities = require('../utilities/utilities');
 
+/**
+ * @route POST /documentType
+ * @group Document Tyopes
+ * @param {object} object.body - Document Types's Credentials - eg. {"name": "Faturas"} 
+ * @returns {object} 201 - New Document Type created
+ * @returns {Error} 400 - Unexpected error
+ * @returns {Error} 500 - Internal Server Error
+ */
+
 router.route('/').post([
     body('name').notEmpty().escape(),
 ],function(req, res) { 
@@ -15,6 +24,15 @@ router.route('/').post([
     }
 })
 
+/**
+ * @route GET /documentTypes
+ * @group Document Types
+ * @returns {object} 200 - An array with all Document Types
+ * @returns {Error} 404 - 0 Document Types found!
+ * @returns {Error} 400 - Unexpected error
+ * @returns {Error} 500 - Internal Server Error
+ */
+
 router.route('/').get(function(req, res) {
     const errors = validationResult(req);
     if (errors.isEmpty()) {
@@ -23,6 +41,21 @@ router.route('/').get(function(req, res) {
         res.status(400).send(erros);
     }
 })
+
+/**
+ * @route PUT /documentType/{id}
+ * @group Document Type
+ * @param {object} object.body - Document type's Credentials - eg. {"pending": 1}   //VER AQUI
+ * @param {string} id.path - Document Type id
+ * @returns {object} 200 - Document Type updated with success!
+ * @returns {Error} 400 - Error while updating the Document Type!
+ * @returns {Error} 400 - Only admin and users with access can update Document Types!
+ * @returns {Error} 401 - Invalid Token
+ * @returns {Error} 403 - No token provided
+ * @returns {Error} 404 - Document Type not found
+ * @returns {Error} 500 - Internal Server Error
+ * @security Bearer
+ */
 
 router.route('/:documentTypeId').put([
     body('name').notEmpty().escape(),
@@ -35,6 +68,21 @@ router.route('/:documentTypeId').put([
         res.status(400).send(errors); 
     }
 })
+
+/**
+ * @route PATCH /documentType/{id}
+ * @group Document Type
+ * @param {object} object.body - Document Type's Credentials - eg. {"deleted": 1}
+ * @param {string} id.path - Document Type
+ * @returns {object} 200 - Document Type updated with success!
+ * @returns {Error} 400 - Error while updating the Document Type!
+ * @returns {Error} 400 - Only admin and users with access can update Document Types!
+ * @returns {Error} 401 - Invalid Token
+ * @returns {Error} 403 - No token provided
+ * @returns {Error} 404 - Document Type not found
+ * @returns {Error} 500 - Internal Server Error
+ * @security Bearer
+ */
 
 router.route('/:documentTypeId').patch([
     body('deleted').isNumeric().notEmpty().escape(),
