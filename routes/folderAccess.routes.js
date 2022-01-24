3f4c4e58ed5f568ff12690a9447626a48d6a6e9e
tiagocouto17 <tiago.couto1745@gmail.com>
2022-01-24 14:58:44 +0000

    folder acess swagger

--- routes/folderAccess.routes.js
+++ routes/folderAccess.routes.js
@@ -4,6 +4,15 @@ const { body, validationResult } = require('express-validator');
 const controller = require('../controllers/folderAccess.controller'); 
 const utilities = require('../utilities/utilities');
 
+/**
+ * @route POST /folderAccess
+ * @group Folder Access
+ * @param {object} object.body - Folder Access's Credentials - eg. {"name": "Faturas"} 
+ * @returns {object} 201 - New FolderAccess created
+ * @returns {Error} 400 - Unexpected error
+ * @returns {Error} 500 - Internal Server Error
+ */
+
 router.route('/').post([
     body('userId').isNumeric().notEmpty().escape(),
     body('folderId').isNumeric().notEmpty().escape(),
@@ -17,6 +26,21 @@ router.route('/').post([
         res.status(400).send(errors); 
     }
 })
+
+/**
+ * @route PUT /folderAccess/{id}/{userId}
+ * @group Folder Access
+ * @param {object} object.body - Folder Access's Credentials - eg. {"pending": 1}   //VER AQUI
+ * @param {string} id.path - Folder Access id
+ * @returns {object} 200 - FolderAccess updated with success!
+ * @returns {Error} 400 - Error while updating the FolderAccess!
+ * @returns {Error} 400 - Only admin and users with access can update folderAccess!
+ * @returns {Error} 401 - Invalid Token
+ * @returns {Error} 403 - No token provided
+ * @returns {Error} 404 - Folder Access not found!
+ * @returns {Error} 500 - Internal Server Error
+ * @security Bearer
+ */
 
 router.route('/:folderId/:userId').put([
     body('access').isNumeric().notEmpty().escape(),
